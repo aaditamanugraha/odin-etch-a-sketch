d1da722f6318e953910de56b3191f56216377876
aaditamanugraha <adhityaaditaman@gmail.com>
2023-04-25 15:18:25 +0700

    Finished the algorithm (got some bugs)

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const gridContainer = document.getElementById("container");
+const resetBtn = document.getElementById("reset-btn");
 
-for (let i = 0; i <= 16 * 16; i++) {
-  const square = document.createElement("div");
+function createGrid(size) {
+  gridContainer.innerHTML = "";
 
-  square.classList.add("square");
-  gridContainer.appendChild(square);
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size; i++) {
+    const square = document.createElement("div");
+
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+  }
+
+  gridContainer.querySelectorAll(".square").forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
+  });
 }
+
+createGrid(16);
+
+resetBtn.addEventListener("click", () => {
+  let size = prompt("Masukan grid size (1-100).");
+  if (size !== null && !isNaN(size) && size >= 1 && size <= 100) {
+    createGrid(size);
+  }
+});
